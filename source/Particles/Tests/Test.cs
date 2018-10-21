486aeac35dd892ed50844d7e021b30a58539d3b2
Gereon Fox <fox@depend.uni-saarland.de>
2018-10-21 14:57:52 +0200

    Created a test case for VideoWriter.

--- source/Particles/Tests/Test.cs
+++ source/Particles/Tests/Test.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Xunit;
+using Particles;
+using System.IO;
+using System.Drawing;
 namespace Tests
 {
     public class Test
@@ -9,6 +12,29 @@ namespace Tests
         {
             var m = new Particles.MyClass();
             Assert.Equal(42, m.Test());
+        }
+
+        [Fact()]
+        public void TestVideoWriter()
+        {
+            var file = new FileStream("/tmp/videoWriterTest.mp4", FileMode.Create);
+
+            int w = 800;
+            int h = 600;
+            int fps = 10;
+
+            using (var vw = new VideoWriter(file, VideoCodec.H264, w, h, fps))
+            {
+                for (int s = 0; s < 3; s++)
+                    for (int f = 0; f < fps; f++)
+                    {
+                        var img = new Bitmap(w, h);
+                        using (var g = Graphics.FromImage(img))
+                            g.DrawString(f.ToString(), new Font(FontFamily.GenericMonospace, 60), Brushes.Red, new PointF(0, 0));
+
+                        vw.Append(img);
+                    }
+            }
         }
     }
 }
