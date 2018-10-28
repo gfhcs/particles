4bca1035d2f1a1343d1fc954225d47ec5444e0e2
Gereon Fox <fox@depend.uni-saarland.de>
2018-10-28 22:01:47 +0100

    Added RK4.

---
+++ source/Particles/Particles/integrators/RK4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Particles
+{
+    /// <summary>
+    /// The classical Runge-Kutta method, of order 4.
+    /// </summary>
+    public class RK4<Q, G> : RungeKuttaIntegrator<Q, G> where Q : IDifferentiable<Q, G> where G : IGradient<Q, G>
+    {
+        private static readonly ImmutableArray<long> butcher = new long[] { 0, 0, 0, 0, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 6, 0, 1, 2, 2, 1, 0, 3, 3, 6 }.ToImmutableArray();
+
+        public RK4() : base(butcher, 6)
+        {
+        }
+    }
+}
