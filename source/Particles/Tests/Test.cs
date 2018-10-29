fefb120c472124f38b591b3c638e62012fcf74d7
Gereon Fox <fox@depend.uni-saarland.de>
2018-10-29 21:42:40 +0100

    Fixes & improvements for TestEarthMoon.

--- source/Particles/Tests/Test.cs
+++ source/Particles/Tests/Test.cs
@@ -56,13 +56,13 @@ namespace Tests
             int w = 800;
             int h = 600;
             int r = Math.Min(w, h) / 10;
-            var fps = 60;
+            var fps = 15;
             var visualDuration = 60.0;
             var simulatedDuration = 2 * 30 * 24 * 60 * 60.0;
 
             var state = new MatterCloud(2);
 
-            var stepSize = 60 * 60.0;
+            var stepSize = 24 * 60 * 60.0;
             state.Positions[0] = new Vector3(0, 0, 0);
             state.Positions[1] = new Vector3(362600000, 0, 0);
 
@@ -72,30 +72,31 @@ namespace Tests
             state.Velocities[0] = new Vector3(0, 0, 0);
             state.Velocities[1] = new Vector3(0, 1022, 0);
 
-            var scale = 0.5 * (1.0 / 362600000);
+            var scale = 0.5 * (1.0 / 362600000) * Math.Min(w, h);
 
             var dt = (simulatedDuration / visualDuration) / fps;
 
             var bitmap = new Bitmap(w, h);
 
             using (var vw = new VideoWriter(file, VideoCodec.H264, w, h, fps))
-                for (var sim = new Simulation<MatterCloud, MatterCloudGradient>(state, new RK4<MatterCloud, MatterCloudGradient>(), stepSize); 
+                for (var sim = new Simulation<MatterCloud, MatterCloudGradient>(state, new RK4<MatterCloud, MatterCloudGradient>(), stepSize);
                      sim.Time < simulatedDuration; sim.Advance(dt))
-            {
-                using (var g = Graphics.FromImage(bitmap))
                 {
-                    g.Clear(Color.Black);
-
-                    foreach (var p in sim.State.Positions)
+                    using (var g = Graphics.FromImage(bitmap))
                     {
-                        var x = (int)(scale * p.X);
-                        var y = (int)(scale * p.Y);
-                        g.FillEllipse(Brushes.White, x - r / 2, y - r / 2, r, r);
+                        g.Clear(Color.Black);
+
+                        foreach (var p in sim.State.Positions)
+                        {
+                            var x = w / 2 + (int)(scale * p.X);
+                            var y = h / 2 - (int)(scale * p.Y);
+                            g.FillEllipse(Brushes.White, x - r / 2, y - r / 2, r, r);
+                        }
                     }
+
+                    vw.Append(bitmap);
                 }
 
-                 vw.Append(bitmap);
-            }
 
             vlc(path);
         }
