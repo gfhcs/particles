c114465e3380bff4409046c8e7a590bfd7a819e2
Gereon Fox <fox@depend.uni-saarland.de>
2018-11-02 19:18:45 +0100

    Created DiscreteFilter and BinomialFilter.

---
+++ source/Particles/Particles/BinomialFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Particles
+{
+    /// <summary>
+    /// Represents the convolution of a binomial filter with a physical signal.
+    /// This is an approximation of a Gaussian filter.
+    /// </summary>
+    public class BinomialFilter : DiscreteFilter
+    {
+        /// <summary>
+        /// Returns the <paramref name="n"/>-th line of Pascal's triangle.
+        /// </summary>
+        /// <param name="n">The 1-based index of the line of Pascal's triangle that is to be computed. Must be odd.</param>
+        private static double[] pascal(int n)
+        {
+            if (n % 2 != 1)
+                throw new ArgumentException(string.Format("{0} must be odd!", nameof(n)), nameof(n));
+
+            var line = new double[n];
+
+            var m = n / 2;
+            line[m] = 1;
+
+            for (; n > 0; n--)
+                for (int i = 1; i < m; i++) {
+                    var s = line[m + i - 1] + line[m + i];
+                    line[m + i] = s;
+                    line[m - 1] = s;
+            }
+
+            return line;
+        }
+
+        /// <summary>
+        /// Creates a new binomial filter.
+        /// </summary>
+        /// <param name="width">The width of the convolution kernel.</param>
+        /// <param name="precision">The precision with which a gaussian kernel is to be approximated. Must be positive. Higher values give more precision, but require more computation.</param>
+        /// <param name="argument">The physical signal to filter.</param>
+        public BinomialFilter(double width, int precision, Func<double, double> argument) : base(width, pascal(precision + (1 - precision % 2)).ToImmutableArray(), argument)
+        {
+        }
+    }
+}
---
+++ source/Particles/Particles/DiscreteFilter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Particles
+{
+    /// <summary>
+    /// A mathematical function that is convoluted with a physical signal over the time domain,
+    /// in order to filter the signal.
+    /// </summary>
+    public class DiscreteFilter
+    {
+        private readonly double width;
+        private readonly double delta;
+        private readonly double n;
+        private readonly ImmutableArray<double> kernel;
+        private Func<double, double> argument;
+
+        /// <summary>
+        /// Creates a new discrete filter.
+        /// </summary>
+        /// <param name="width">The width of the convolution kernel.</param>
+        /// <param name="kernel">A discrete representation of the convolution kernel. Its length must be odd.</param>
+        /// <param name="argument">The physical signal to filter.</param>
+        protected DiscreteFilter(double width, ImmutableArray<double> kernel, Func<double, double> argument)
+        {
+            if (kernel.Length % 2 != 1)
+                throw new ArgumentException("The length of the kernel must be odd!", nameof(kernel));
+            this.width = width;
+            this.kernel = kernel;
+            this.argument = argument;
+            this.delta = width / (Math.Max(1, kernel.Length - 1));
+            this.n = kernel.Sum();
+        }
+
+        /// <summary>
+        /// Evaluates the convolution of this filter with its argument signal.
+        /// </summary>
+        /// <returns>The value of the convolution for the given <paramref name="x"/>.</returns>
+        /// <param name="x">A point at which the argument signal is defined.</param>
+        public double Evaluate(double x)
+        {
+            var e = 0.0;
+            for (int i = 0; i < kernel.Length; i++)
+                e += kernel[i] * argument(x + (i - kernel.Length / 2) * delta);
+
+            return e / n;
+        }
+    }
+
+}
