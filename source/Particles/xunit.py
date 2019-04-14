1f9fba417f94e0c0851199b1b954a60e67a48940
Gereon Fox <fox@mpi-inf.mpg.de>
2019-04-14 22:49:07 +0200

    Some adjustments of the XUnit script for the purposes of this repository.

--- source/Particles/xunit.py
+++ source/Particles/xunit.py
@@ -19,24 +19,35 @@ parser.add_argument('-nc', '--no-color', dest='nocolor', action='store_true', he
 parser.add_argument(dest='assemblies', type=str, nargs='+',
                 help='An assembly to search for XUnit test cases')
 
-parser.add_argument('-c', '--class', dest='classes', type=str, default=[], nargs='*',
-                help='Executes all the test cases of the class with the given, fully qualified name (including namespace!). If this is never given, *all* test cases of all given assemblies are executed.')
+parser.add_argument('-c', '--class', dest='classes', type=str, default=[], action='append',
+                help='Limit execution to  the test cases of the class with the given, fully qualified name (including namespace!).')
+parser.add_argument('-m', '--method', dest='methods', type=str, default=[], action='append',
+                help='Limit execution to the specified test method. The name must be fully qualified (including namespace!), but may contain wildcards (*).')
+
 
 args = parser.parse_args()
 
 monoArgs = ['mono']
 if args.profile:
-    monoArgs.append('--profile=log:report')
+    monoArgs.append('--profile=log:calls,output=profile.mlpd')
 monoArgs.append('packages/xunit.runner.console.2.4.1/tools/net461/xunit.console.exe')
-for a in args.assemblies:
-    monoArgs.append('Binaries/Debug/{assembly}.dll'.format(assembly=a))
+monoArgs.extend(args.assemblies)
 for cn in args.classes:
     monoArgs.append('-class')
     monoArgs.append(cn)
+for mn in args.methods:
+    monoArgs.append('-method')
+    monoArgs.append(mn)
     
 monoArgs.append('-verbose')
 
 if args.nocolor:
     monoArgs.append('-nocolor')
 
-sys.exit(subprocess.call(monoArgs, env={'LD_LIBRARY_PATH' : os.path.join(root, 'Binaries/Debug')}))
+exitCode = subprocess.call(monoArgs, env={'LD_LIBRARY_PATH' : os.path.join(root, 'Binaries/Debug')})
+
+if args.profile and exitCode == 0:
+    exitCode=subprocess.call(['mprof-report', '--out=profile.txt', 'profile.mlpd'])
+    subprocess.call(['rm', 'profile.mlpd'])
+
+sys.exit(exitCode)
